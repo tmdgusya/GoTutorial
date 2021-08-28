3af98d5ebb1e0bab1086989620a53ef6fd466f37
roach <tmdgusya@naver.com>
2021-08-29 00:08:53 +0900

    request

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
-	"roach/banking"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request Failed")
+
 func main() {
-	account := banking.NewAccount("roach")
-	account.Deposit(1000)
-	err := account.WithDraw(10000);
-	if err != nil {
-		log.Fatalln(err)
+	urls := []string {
+		"https://www.naver.com/",
 	}
-	fmt.Println(account.Balance())
+
+	for _, url := range urls {
+		resp, _ := hitURL(url)
+		fmt.Println("Response", resp)
+	}
 }
+
+func hitURL(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400{
+		return nil, errRequestFailed
+	}
+	return resp, nil
+} 
